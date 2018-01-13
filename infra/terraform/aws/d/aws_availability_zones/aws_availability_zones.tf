8a57e0bf7844390951389cb70902903954626565
Yoshitaka Nishimura <nishimuray@gmail.com>
2018-01-13 23:51:02 +0900

    terraform data aws_availability_zones

---
+++ infra/terraform/aws/d/aws_availability_zones/aws_availability_zones.tf
@@ -0,0 +1,11 @@
+# [AWS: aws_availability_zones - Terraform by HashiCorp](https://www.terraform.io/docs/providers/aws/d/availability_zones.html)
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+output "aws_availability_zones" {
+  value = "${data.aws_availability_zones.available.names}"
+}
+
+# ap-northeast-1 の場合に 1b が使えるアカウントがある（古いアカウントだ）
