28a60d4e34fbbeff79f2477545c62609f0dcd7cf
Yoshitaka Nishimura <nishimuray@gmail.com>
2018-01-28 14:17:16 +0900

    feat: add functions, chomp()

---
+++ infra/terraform/configuration/functions/chomp.tf
@@ -0,0 +1,26 @@
+variable "chomp_straing_1" {
+  default = " space "
+}
+
+output "chomp_string_1" {
+  description = "no removes"
+  value       = "\"${chomp(var.chomp_straing_1)}\""
+}
+
+variable "chomp_straing_2" {
+  default = "new line\nand new line"
+}
+
+output "chomp_string_2" {
+  description = "no removes not trail"
+  value       = "\"${chomp(var.chomp_straing_2)}\""
+}
+
+variable "chomp_straing_3" {
+  default = "new line\nand new line\n\n\n"
+}
+
+output "chomp_string_3" {
+  description = "only removes all trailing newline"
+  value       = "\"${chomp(var.chomp_straing_3)}\""
+}
